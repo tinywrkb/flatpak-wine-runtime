3315784f34b8a3bcd39105dafce37475413dddfd
tinywrkb <tinywrkb@gmail.com>
2021-11-25 00:44:38 +0200

    wineasio: upstream is now building the fake dlls

--- wineasio/build.sh
+++ wineasio/build.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-WINEBUILD=winebuild
+export WINEBUILD=winebuild
 export WINECC=winegcc
 
 if [ "$CC" == 'i686-unknown-linux-gnu-gcc' ]; then
   _ARCH='i386'
   _ML='32'
-  WINEBUILD+=" --target i686-unknown-linux-gnu"
+  # since 4b4f68165f963a7396118817fc1a97232782e1b2 there's no need to set winebuild target
+  #export WINEBUILD+=" --target i686-unknown-linux-gnu"
   export WINECC+=" --target i686-unknown-linux-gnu"
 else
   _ARCH='x86_64'
@@ -19,6 +20,5 @@ export WINEARCH=win${_ML}
 wineprefix=${FLATPAK_BUILDER_BUILDDIR}/wine
 
 make ${_ML}
-install -Dm644 build${_ML}/wineasio.dll.so -t ${FLATPAK_DEST}/lib/${_ARCH}-linux-gnu/wine/${_ARCH}-unix/
-$WINEBUILD --dll --fake-module -E wineasio.dll.spec build${_ML}/{asio,main,regsvr}.c.o > build${_ML}/wineasio.dll
-install -Dm644 build${_ML}/wineasio.dll -t ${FLATPAK_DEST}/lib/${_ARCH}-linux-gnu/wine/${_ARCH}-windows/
+install -Dm755 build${_ML}/wineasio.dll.so -t ${FLATPAK_DEST}/lib/${_ARCH}-linux-gnu/wine/${_ARCH}-unix/
+install -Dm755 build${_ML}/wineasio.dll -t ${FLATPAK_DEST}/lib/${_ARCH}-linux-gnu/wine/${_ARCH}-windows/
